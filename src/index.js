58cee61b2ab12705cad4f6080c760df5bc66439d
Brian Tom <btom.0831@gmail.com>
2017-10-02 20:57:57 -0700

    Update index.js with Hello World

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+
+    return (
+        // JSX goes here
+        <div>
+        <h1>Hello World!</h1>
+        <p>This is a random paragraph.</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
 registerServiceWorker();
